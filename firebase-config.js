e3da633faf6f4e4ad7925a2504b468a316f8c7a7
Jana Albader <159451008+janaalbader28@users.noreply.github.com>
2025-03-15 23:46:08 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyB0COTA1axQug42cvD5f_ym5CWPPbH3YYU",
+    authDomain: "autismlens-267e5.firebaseapp.com",
+    projectId: "autismlens-267e5",
+    storageBucket: "autismlens-267e5.firebasestorage.app",
+    messagingSenderId: "44492084243",
+    appId: "1:44492084243:web:cd92a7f4bc8db5680d9be6"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
